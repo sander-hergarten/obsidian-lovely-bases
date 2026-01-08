89fecc204f400e668bd61d07028d5bd68a40f4a2
aitorllamas <aitorllj93@gmail.com>
2026-01-08 13:07:56 +0100

    add missing internal variables

--- src/components/InfiniteDragScroll.tsx
+++ src/components/InfiniteDragScroll.tsx
@@ -42,7 +42,7 @@ const gridItemStyles = cva(
 				default: "rounded-sm",
 				masonry: "rounded-sm",
 				polaroid:
-					"border-10 border-b-28 border-white shadow-xl even:rotate-3 odd:-rotate-2 hover:rotate-0 transition-transform ease-out duration-300",
+					"border-10 border-b-28 border-card shadow-xl even:rotate-3 odd:-rotate-2 hover:rotate-0 transition-transform ease-out duration-300",
 			},
 		},
 		defaultVariants: {
